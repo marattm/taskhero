25c64c0a01a303c813751bd3d57c24dfa0324913
marattm <mtulepbayevmonnie@hawk.iit.edu>
2018-05-08 12:20:27 -0500

    Postgre configuration

--- services/users/cmd.sh
+++ services/users/cmd.sh
@@ -10,3 +10,12 @@ python manage.py runserver
 # docker
 sudo docker-compose -f docker-compose-dev.yml build
 sudo docker-compose -f docker-compose-dev.yml up -d
+docker-compose -f docker-compose-dev.yml up -d --build # sanity check
+docker-compose -f docker-compose-dev.yml \
+  run users python manage.py recreate_db
+
+    # postgre
+    docker exec -ti $(docker ps -aqf "name=users-db") psql -U postgres
+    \c users_dev
+    \dt
+    \q
