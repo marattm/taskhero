4ed8baadd478c87753ddad4da470df0860586672
marattm <mtulepbayevmonnie@hawk.iit.edu>
2018-05-22 18:55:13 -0500

    DB migration command line works but still issues wth the migration part

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f Docker-compose-dev.yml \ run users python manage.py db init
-docker-compose -f Docker-compose-dev.yml \ run users python manage.py db migrate
-docker-compose -f Docker-compose-dev.yml \ run users python manage.py db upgrade
+docker-compose -f Docker-compose-dev.yml run users python manage.py db init
+docker-compose -f Docker-compose-dev.yml run users python manage.py db migrate
+docker-compose -f Docker-compose-dev.yml run users python manage.py db upgrade
