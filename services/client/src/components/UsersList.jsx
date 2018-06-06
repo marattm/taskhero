6b1ca4dcf2d47be61be51035afd9d6c73adf9cf6
marattm <mtulepbayevmonnie@hawk.iit.edu>
2018-06-06 10:38:44 -0500

    Client UI imporve for readability

--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -9,7 +9,21 @@ const UsersList = (props) => {
                         <h4
                             key={user.id}
                             className="card card-body bg-light"
-                        >{user.username}, {user.email}, {user.first_name}, {user.last_name}, {user.bio}, {user.gender}, {user.created}
+                        >
+                        <ul>
+                                <li><em>active:</em>{user.active}</li>
+                                <li><em>bio:</em>{user.bio}</li>
+                                <li><em>change_configuration:</em>{user.change_configuration}</li>
+                                <li><em>created:</em>{user.created}</li>
+                                <li><em>email:</em>{user.email}</li>
+                                <li><em>email_confirmed:</em>{user.email_confirmed}</li>
+                                <li><em>first_name:</em>{user.first_name}</li>
+                                <li><em>gender:</em>{user.gender}</li>
+                                <li><em>id:</em>{user.id}</li>
+                                <li><em>last_name:</em>{user.last_name}</li>
+                                <li><em>profile_image:</em>{user.profile_image}</li>
+                                <li><em>username:</em>{user.username}</li>
+                        </ul>
                         </h4>
                     )
                 })
